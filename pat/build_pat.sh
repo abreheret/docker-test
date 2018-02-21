095b1d7dda66bf3c25a9a88fc31c4249d8a22c21
Amaury Bréhéret <abreheret@gmail.com>
2018-02-21 15:14:03 +0100

    update , works to build on ubuntu14.04

---
+++ pat/build_pat.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd root
+export CMAKE_PREFIX_PATH=/opt/qt59
+git clone https://github.com/abreheret/PixelAnnotationTool.git
+mkdir build
+cd build
+cmake ../PixelAnnotationTool -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
+cmake --build . --config Release
